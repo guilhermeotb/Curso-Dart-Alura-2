23e176027a2a1a7b58f24be9e0582332b4c0fa6f
Pereira <pereira@eb.mil.br>
2026-02-18 16:54:57 -0300

    #1 criação da função base  e set de emails

--- email/bin/email.dart
+++ email/bin/email.dart
@@ -1,5 +1,40 @@
-import 'package:email/email.dart' as email;
 
 void main() {
+/*  List<String> listaEmails = ['joao@gmail.com', 'maria@gmail.com' ];
+
+ listaEmails.add('enzo@gmail.com');
+ 
+ if(listaEmails.contains('enzo@gmail.com') == false){
+    listaEmails.add('enzo@gmail.com');
+ }
+ 
+for( String email in listaEmails){
+    enviarEmail(email);
+} */
+
+Set<String> setEmails = {'joao@gmail.com', 'maria@gmail.com'};
+
+
+setEmails.add('enzo@gmail.com');
+
+
+
+for(String email in setEmails){
+    print(email);
+}
+
+
+if(setEmails.contains('enzo@gmail.com') == true ){
+    print('Email do enzo dentro do set');
+} else{
+    print('Não está no set');
+}
   
 }
+
+
+
+void enviarEmail(String email){
+    print('mensagem enviada para: $email');
+}
+
